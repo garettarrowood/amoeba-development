49ef6042f77fd5327637025a0f5600a9cc004861
garettarrowood <garettarrowood@gmail.com>
2015-09-30 15:11:40 -0400

    starting view setup

--- app/controllers/amoebas_controller.rb
+++ app/controllers/amoebas_controller.rb
@@ -3,5 +3,13 @@ class AmoebasController < ApplicationController
   def main
     render 'main'
   end
-  
+
+  def index
+    @amoebas = Amoeba.all
+  end
+
+  def new
+    @amoeba = Amoeba.new
+  end
+
 end
