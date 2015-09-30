f2641aaa5f13234ccbebf0add7cc89060de09bce
garettarrowood <garettarrowood@gmail.com>
2015-09-30 12:48:30 -0400

    again

---
+++ app/models/test.rb
@@ -0,0 +1,24 @@
+class Test
+
+  def spacing
+    if i_dont_ever_press
+      tab
+    end
+  end
+
+  def tabbing
+    oops tab is set to 4 on this
+  end
+
+  def last
+      where will this show up?
+        this is one tab farther than the above
+          and so is this
+  end
+
+
+
+
+
+
+end
